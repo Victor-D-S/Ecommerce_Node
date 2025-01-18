8cda575be05c24b39c1db9a6453d8d0cc97fb82a
Victor da Silva <victordasilva14@yahoo.com>
2025-01-18 20:08:29 -0300

    :collision: Validação de erros

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,27 +1,38 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { prismaClient } from "..";
 import { hashSync, compareSync } from "bcrypt";
 import * as jwt from 'jsonwebtoken';
 import { JWT_SECRET } from "../secrets";
+import { BadRequestException } from "../exceptions/bad-requests";
+import { ErrorCode } from "../exceptions/root";
+import { UnprocessableEntity } from "../exceptions/validation";
+import { SignUpSchema } from "../schema/users";
 
-export const signup = async (req: Request, res: Response) => {
-    const { email, password, name } = req.body;
+export const signup = async (req: Request, res: Response, next:NextFunction) => {
 
-    let user = await prismaClient.user.findFirst({where: {email}});
+    try {
+        
+        SignUpSchema.parse(req.body);
+        const { email, password, name } = req.body;
 
-    if(user) {
-        throw Error('User already exists');
-    }
-
-    user = await prismaClient.user.create({
-        data: {
-            email,
-            password: hashSync(password, 10),
-            name
+        let user = await prismaClient.user.findFirst({where: {email}});
+    
+        if(user) {
+            next(new BadRequestException('User already exists', ErrorCode.USER_ALREADY_EXISTS));
         }
-    })
-
-    res.json(user);
+    
+        user = await prismaClient.user.create({
+            data: {
+                email,
+                password: hashSync(password, 10),
+                name
+            }
+        })
+    
+        res.json(user);
+    } catch (err:any) {
+        next(new UnprocessableEntity(err?.issues, 'Unprocessable Entity', ErrorCode.UNPROCESSABLE_ENTITY));
+    }
 }
 
 export const login = async (req: Request, res: Response) => {
