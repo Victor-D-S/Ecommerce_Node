f7bb9e02caaa5b29666379b62e27fd71c86c08dc
Victor da Silva <victordasilva14@yahoo.com>
2025-01-26 16:20:56 -0300

    Rotas de Produtos

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler";
-import { createProduct } from "../controllers/products";
+import { createProduct, deleteProduct, getProductById, listProducts, updateProduct } from "../controllers/products";
+import authMiddleware from "../middlewares/auth";
+import adminMiddleware from "../middlewares/admin";
 
 const productsRouter:Router = Router();
 
-productsRouter.post('/', errorHandler(createProduct));
+productsRouter.post('/', [authMiddleware, adminMiddleware], errorHandler(createProduct));
+productsRouter.put('/:id', [authMiddleware, adminMiddleware], errorHandler(updateProduct));
+productsRouter.delete('/:id', [authMiddleware, adminMiddleware], errorHandler(deleteProduct));
+productsRouter.get('/', [authMiddleware, adminMiddleware], errorHandler(listProducts));
+productsRouter.get('/:id', [authMiddleware, adminMiddleware], errorHandler(getProductById));
 
 export default productsRouter;
